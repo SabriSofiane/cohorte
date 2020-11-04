00bf82dcbb5fee077655ddc983a719d05dabdefa
SabriSofiane <hass.sabri@gmail.com>
2020-11-04 10:39:24 +0100

    update menu

--- menu.php
+++ menu.php
@@ -1,4 +1,24 @@
 <!--Menu du site web-->
+<?php 
+    
+    function contains($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle !== '' && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
+        return false;
+    }
+  
+    $racine = './';
+    $repertoire = array('administration' , 'support' );  // les répertoires de premier niveau du site
+	if  (contains($_SERVER['PHP_SELF'], $repertoire)){
+		$racine = '../';
+	}
+	
+
+?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="index.php">LPO Touchard - Washington</a>
@@ -11,11 +31,12 @@
                         <li class="nav-item dropdown">
                             <a class="nav-link  dropdown-toggle" data-toggle="dropdown">Menu</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="formulaire.php">Effectuer son suivi</a></li>
-                                <li><a class="dropdown-item" href="contacter.php">Contacter</a></li>
-                                <li><a class="dropdown-item" href="informations.php">Mentions légales</a></li>
+                                <li><a class="dropdown-item" href="<?php echo $racine ?>formulaire.php">Effectuer son suivi</a></li>
+                                <li><a class="dropdown-item" href="<?php echo $racine ?>contacter.php">Contacter</a></li>
+                                <li><a class="dropdown-item" href="<?php echo $racine ?>informations.php">Mentions légales</a></li>
                             </ul>
                         </li>
+                        <li class="nav-item"><a class="nav-link" href="<?php echo $racine ?>administration/vue_eleves.php">Administration</a></li>
                     </ul>
                 </div> <!-- navbar-collapse.// -->
 
